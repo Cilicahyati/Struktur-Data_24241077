87fe529bf585fa21522d406e097015ecf41f4fa0
Cili Cahyati <cilychyati27@gmail.com>
2025-04-16 17:43:29 +0700

    Add files via upload

---
+++ Struktur Data_24241077/Pre-Praktikum/for_loop.py
@@ -0,0 +1,52 @@
+# PERULANGAN
+# FOR LOOP
+'''
+ for-loop digunakan untuk perulangan
+       yang diketahui jumlah perulangannya
+       Counted-loop 
+
+       syntax:
+       for kondisi:
+         aksi 1 - bagian dari loop
+      aksi 2 - bukan bagian dari loop 
+
+'''
+
+a = 0
+a += 1
+print(a)
+a += 1
+print(a)
+a += 1
+print(a)
+# boros line of code
+
+angka = [0,1,5,7,10]
+for i in angka:
+    print(f"i saat ini -> {i}")
+print("ini akhir dari loop\n")
+
+
+
+#perulangan menggunakan range 
+angka_range = range(5) # diulang 5 kali
+print(list(angka_range))
+
+for i in angka_range:
+    print(f"i saat ini->{i}")
+print("ini akhir dari loop 2\n")
+
+angka2_range = range(1,10) # di ulang 9 kali
+print(list(angka2_range))
+
+for i in angka2_range:
+    print(f"saat ini -> (i)")
+print("ini akhir dari loop 3\n")
+
+#perulangan menggunakan string
+string ="Mandalika"
+print(string)
+
+for i in string:
+    print(f"i saat ini -> {i}")
+print("ini akhir dari loop 4\n")
